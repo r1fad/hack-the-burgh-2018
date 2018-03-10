7786806e699326384aa03994af8ef52bcf42fe5c
Rifad Lafir <rifadahmedlafir@gmail.com>
2018-03-10 23:06:27 +0000

    add patient profile

--- blink/src/components/App.js
+++ blink/src/components/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import '../styles/app.css'
 
-import UserInfo from './UserInfo';
+import MenuAppBar from './MenuAppBar';
 
 class App extends Component{
 
   render(){
+    const {name, age, id} = this.props.userInfo;
     return(
       <div>
-        <UserInfo/>
+        <MenuAppBar name={name} age={age} id={id} />
       </div>
     )
   }
 
 }
 
-export default App;
+function mapStateToProps(state){
+  return state;
+}
+
+export default connect(mapStateToProps, null)(App);
---
+++ blink/src/components/MenuAppBar.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Switch from 'material-ui/Switch';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+import Menu, { MenuItem } from 'material-ui/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class MenuAppBar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes, name, age, id } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" style={{backgroundColor: '#cf4c18'}}>
+          <Toolbar>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              BL!NK Patient Profile
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>{name}</MenuItem>
+                  <MenuItem onClick={this.handleClose}>{age}</MenuItem>
+                  <MenuItem onClick={this.handleClose}>{id}</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+MenuAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MenuAppBar);
