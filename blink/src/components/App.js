cd1c5d353c0f8d6001b828440be14d1bba76166c
Hendrik Mölder <hendrik@molder.eu>
2018-03-11 08:31:31 +0000

    Add graph

--- blink/src/components/App.js
+++ blink/src/components/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import '../styles/app.css'
+import '../styles/app.css';
+import Card, { CardHeader } from 'material-ui/Card';
+import Grid from 'material-ui/Grid'
+import { AreaChart, Area, YAxis } from 'recharts';
+import { values, mapValues } from 'lodash';
 
 import MenuAppBar from './MenuAppBar';
+import ResponsiveContainer from 'recharts/lib/component/ResponsiveContainer';
 
 class App extends Component{
 
@@ -16,24 +21,57 @@ class App extends Component{
   componentDidMount(){
     const ele = document.getElementById('ipl-progress-indicator')
     if (ele){
-      fetch('http://127.0.0.1:5000/userdata',{
+      fetch('https://hacktheburgh-186fa.firebaseio.com/processed.json',{
         method: 'get',
         mode: 'cors'
       })
       .then(response => response.json())
-      .then(data=>this.setState({data:data, isLoading: false}))
+      .then(jsonData => values(jsonData))
+      .then(jsonData => jsonData.map((obj, index) => {
+        return { interval: index, count: parseInt(obj.count) }
+      }))
+      .then(data =>this.setState({ data: data, isLoading: false }))
+      .then(blabla => console.log(this.state))
       .then(() => ele.outerHTML = '')
     }
   }
 
   render(){
-    const { userData, eyeStats } = this.state.data;
+    // const { userData, eyeStats } = this.state.data;
     if (this.state.isLoading){
       return null;
     }
+
+    const { data } = this.state;
+
     return(
-      <div>
-        <MenuAppBar data={userData} />
+      <div style={{ flexGrow: 1 }}>
+        <MenuAppBar data={{ user: 'test', age: '20', id: '0' }} />
+        <Grid container spacing={8}>
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader
+                title='Blinks'
+                subtitle='Per 10 Second Interval'
+              />
+              <ResponsiveContainer width='100%' height={500}>
+                <AreaChart data={data}
+                  margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
+                >
+                  <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                  </linearGradient>
+                  </defs>
+
+                  <YAxis datakey='count' />
+                  <Area type='monotone' dataKey='count' stroke='#8884d8' fillOpacity={1} fill='url(#colorUv)' />
+                </AreaChart>
+              </ResponsiveContainer>
+            </Card>
+          </Grid>
+        </Grid>
       </div>
     )
   }
